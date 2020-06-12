61e4a2026ca789259ebf4097ef1c1014a0565131
Tiffany Lam <tlam5@sbcglobal.net>
2020-06-12 16:01:47 -0700

    Add middleware for logging requests and error handling

---
+++ server/utils/middleware.js
@@ -0,0 +1,29 @@
+const logger = require('./logger');
+
+const requestLogger = (req, res, next) => {
+  logger.info('Method:', req.method);
+  logger.info('Path', req.path);
+  logger.info('Body', req.body);
+  logger.info('---');
+  next();
+}
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'Unknown endpoint' });
+};
+
+const errorHandler = (error, req, res, next) => {
+  logger.error(error.message);
+
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return res.status(400).send({ error: 'Malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message });
+  } else if (error.name === 'JsonWebTokenError') {
+    return res.status(401).json({ error: 'Invalid token' });
+  }
+
+  next(error);
+}
+
+module.exports = { requestLogger, unknownEndpoint, errorHandler };
